aa94de0ee7cd31abe24820877638cecfbc9cf359
wangsp <spwang@live.cn>
2019-12-27 18:01:59 +0800

    2019-12-27 18:01:56

--- service-db/src/main/java/com/wangspframework/cloud/servicedb/entity/User.java
+++ service-db/src/main/java/com/wangspframework/cloud/servicedb/entity/User.java
@@ -1,6 +1,7 @@
 package com.wangspframework.cloud.servicedb.entity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
@@ -8,14 +9,14 @@ import javax.persistence.*;
  * @author spwang Created on 2019/11/25 at 20:19
  * @version 1.0.0
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Table
 @Entity
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+public class User extends BaseEntity{
+
     private String username;
+    private String account;
     private String password;
     private String phone;
     private String email;
--- service-db/src/main/resources/data/user.sql
+++ service-db/src/main/resources/data/user.sql
@@ -1,1 +1,14 @@
-select * from user;
+CREATE TABLE `user`
+(
+    `id`        int(11)      NOT NULL AUTO_INCREMENT,
+    `username`  varchar(16)  NULL,
+    `account`   varchar(6)   NULL,
+    `password`  varchar(255) NULL,
+    `eamil`     varchar(255) NULL,
+    `phone`     varchar(11)  NULL,
+    `sex`       int          NULL,
+    `create_at` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '' 创建时间 '',
+    `update_at` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '' 更新时间 '',
+    `status`    int(1)       NOT NULL DEFAULT 0 COMMENT '' 0表示未被删除 '',
+    PRIMARY KEY (`id`)
+);
